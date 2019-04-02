7e646f27816866b289457a500ca0c4965a960c73
Utsav Munendra <utsavm9@gmail.com>
2019-04-02 16:25:14 -0700

    ?

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React, { Component } from 'react';
+import React, { Component }
+from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
+
+    function changeNum() {
+        console.log(Math.floor(Math.random() * 1000))
+    }
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <div className = "App">
+            <header className = "App-header">
+                <img src = { logo } className = "App-logo" alt = "logo" onClick={this.changeNum} />
+                <p>{ Math.floor(Math.random() * 1000) }</ p>
+            </ header>
+        </div>
     );
   }
 }
